2d6b601034d46ed46185e8d096678b286c92fc4d
oleg-berger <126603656+oleg-berger@users.noreply.github.com>
2023-11-29 20:26:26 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+static void TripleNumberFinder()
+{
+int[] arrayA = new int[5];
+int[] arrayOp = new int[arrayA.Length];
+for(int i = 0; i < arrayA.Length; i++)
+{
+    Console.Write("Ведите число ");
+    arrayA[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int j = 0; j < arrayA.Length; j++)
+{
+    if(arrayA[j] % 3 == 0)
+    {
+        arrayOp[j] = arrayA[j];
+        Console.Write(arrayOp[j] + " ");
+    }
+    else
+    {
+        continue;
+    }
+}
+}
+
+TripleNumberFinder();
